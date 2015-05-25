63a0c6e5719623003e578f1c4a386a1dcfa7d071
Dan Shechter <dans@eaglets.co.il>
2015-05-25 20:34:11 +0300

    New lzma (still no relevant src drop), new lz4 cmake patches

--- extsrc/lz4-r129/lib/lz4conf.h
+++ extsrc/lz4-r129/lib/lz4conf.h
@@ -1,24 +1,19 @@
 #ifndef LZCONF_H
 #define LZCONF_H
 
-#if defined(WINDOWS) || defined(WIN32)
    /* If building or using as a DLL, define LZ4_DLL.
     * This is not mandatory, but it offers a little performance increase.
     */
+#ifdef WIN32
 #  ifdef LZ4_DLL
-#    if defined(WIN32)
-#      ifdef LZ4_INTERNAL
-#        define LZ4_EXTERN extern __declspec(dllexport)
-#      else
-#        define LZ4_EXTERN extern __declspec(dllimport)
-#      endif
-#    endif
-#  endif  /* LZ4_DLL */
-#endif
+#    define LZ4_EXTERN extern __declspec(dllexport)
+#  else
+#    define LZ4_EXTERN extern __declspec(dllimport)
+#  endif /* LZ4_DLL */
+#endif /* WIN32 */
 
 #ifndef LZ4_EXTERN
 #  define LZ4_EXTERN extern
 #endif
-
 
 #endif /* LZCONF_H */
