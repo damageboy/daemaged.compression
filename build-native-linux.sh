12ee4a9784ac64f821cd2d26be53d53f29ac7f9a
Dan Shechter <dans@houmus.org>
2018-12-26 09:19:44 +0200

    Rearrange build script

--- build-native-linux.sh
+++ build-native-linux.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -x
 PROJECT_ROOT=$(readlink -f $(dirname $0))
 
-DEFAULT_CC=gcc
-X86_SFX="-m32"
-X64_SFX="-m64"
+CC=gcc
+CC_X86="$CC -m32"
+CC_X64="$CC -m64"
 
 X86_OUT=$PROJECT_ROOT/runtimes/${RID}-x86/native/
 X64_OUT=$PROJECT_ROOT/runtimes/${RID}-x64/native/
@@ -17,55 +17,71 @@ if [[ "$GCC_MAJOR" -gt "4" ]]; then
   DEFAULT_FLAGS="${DEFAULT_FLAGS} -flto"
 fi
 
-LIBLZO2DIR=extsrc/lzo2
-(cd $LIBLZO2DIR && git clean -fdx &&
- cp $PROJECT_ROOT/native-cmakes/CMakeLists.lzo2.txt CMakeLists.txt &&
- mkdir build && cd build &&
- CC="$DEFAULT_CC $X86_SFX" cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_C_FLAGS_RELEASE="$DEFAULT_FLAGS" && make -j8 &&
- cp liblzo2.so $X86_OUT)
-(cd $LIBLZO2DIR && git clean -fdx &&
- mkdir build && cd build &&
- cp $PROJECT_ROOT/native-cmakes/CMakeLists.lzo2.txt CMakeLists.txt &&
- CC="$DEFAULT_CC $X64_SFX" cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_C_FLAGS_RELEASE="$DEFAULT_FLAGS" && make -j8 &&
- cp liblzo2.so $X64_OUT)
+function compile_lzo2 {
+  CC_PARAM=$1
+  OUT_PARAM=$2
 
-LIBBZ2DIR=extsrc/bzip2
-(cd $LIBBZ2DIR && git clean -fdx &&
- cp $PROJECT_ROOT/native-cmakes/CMakeLists.bzip2.txt CMakeLists.txt &&
- CC="$DEFAULT_CC $X86_SFX" cmake . -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_C_FLAGS_RELEASE="$DEFAULT_FLAGS" && make -j8 &&
- cp libbz2.so $X86_OUT)
-(cd $LIBBZ2DIR && git clean -fdx &&
- cp $PROJECT_ROOT/native-cmakes/CMakeLists.bzip2.txt CMakeLists.txt &&
- CC="$DEFAULT_CC $X64_SFX" cmake . -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_C_FLAGS_RELEASE="$DEFAULT_FLAGS" && make -j8 &&
- cp libbz2.so $X64_OUT)
+  (cd extsrc/lzo2 && git clean -fdx &&
+   cp $PROJECT_ROOT/native-cmakes/CMakeLists.lzo2.txt CMakeLists.txt &&
+   mkdir build && cd build &&
+   CC="$CC_PARAM" cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_C_FLAGS_RELEASE="$DEFAULT_FLAGS" && make -j8 &&
+   cp liblzo2.so $OUT_PARAM)
+}
 
-LZ4DIR=extsrc/lz4/contrib/cmake_unofficial
-(cd $LZ4DIR && git clean -fdx &&
- cp $PROJECT_ROOT/native-cmakes/CMakeLists.lz4.txt CMakeLists.txt &&
- CC="$DEFAULT_CC $X86_SFX" cmake . -DBUILD_SHARED_LIBS=ON -DBUILD_STATIC_LIBS=OFF -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_C_FLAGS_RELEASE="$DEFAULT_FLAGS" && make -j8 &&
- cp liblz4.so $X86_OUT)
-(cd $LZ4DIR && git clean -fdx &&
- cp $PROJECT_ROOT/native-cmakes/CMakeLists.lz4.txt CMakeLists.txt &&
- CC="$DEFAULT_CC $X64_SFX" cmake . -DBUILD_SHARED_LIBS=ON -DBUILD_STATIC_LIBS=OFF -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_C_FLAGS_RELEASE="$DEFAULT_FLAGS" && make -j8 &&
- cp liblz4.so $X64_OUT)
+function compile_bzip2 {
+  CC_PARAM=$1
+  OUT_PARAM=$2
 
-LIBZDIR=extsrc/zlib-ng
-(cd $LIBZDIR && git clean -fdx &&
- cp $PROJECT_ROOT/native-cmakes/CMakeLists.libz.txt CMakeLists.txt &&
- CC="$DEFAULT_CC $X86_SFX" cmake . -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_C_FLAGS_RELEASE="$DEFAULT_FLAGS" -DZLIB_COMPAT=1 -DARCH=i686 && make -j8 &&
- cp libz.so $X86_OUT)
-(cd $LIBZDIR && git clean -fdx &&
- cp $PROJECT_ROOT/native-cmakes/CMakeLists.libz.txt CMakeLists.txt &&
- CC="$DEFAULT_CC $X64_SFX" cmake . -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_C_FLAGS_RELEASE="$DEFAULT_FLAGS" -DZLIB_COMPAT=1 -DARCH=amd64 && make -j8 &&
- cp libz.so $X64_OUT)
+  (cd extsrc/bzip2 && git clean -fdx &&
+   cp $PROJECT_ROOT/native-cmakes/CMakeLists.bzip2.txt CMakeLists.txt &&
+   CC="$CC_PARAM" cmake . -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_C_FLAGS_RELEASE="$DEFAULT_FLAGS" && make -j8 &&
+   cp libbz2.so $OUT_PARAM)
+}
 
-LIBLZMADIR=extsrc/xz
-(cd $LIBLZMADIR && git clean -fdx &&
- ./autogen.sh && CC="$DEFAULT_CC $X86_SFX" CFLAGS="$DEFAULT_FLAGS" ./configure --host=i686-pc-linux-gnu && make V=0 -j8 &&
- cp src/liblzma/.libs/liblzma.so $X86_OUT)
-(cd $LIBLZMADIR && git clean -fdx &&
- ./autogen.sh && CC="$DEFAULT_CC $X64_SFX" CFLAGS="$DEFAULT_FLAGS" ./configure && make V=0 -j8 &&
- cp src/liblzma/.libs/liblzma.so $X64_OUT)
+function compile_lz4 {
+  CC_PARAM=$1
+  OUT_PARAM=$2
+
+  (cd extsrc/lz4/contrib/cmake_unofficial && git clean -fdx &&
+   cp $PROJECT_ROOT/native-cmakes/CMakeLists.lz4.txt CMakeLists.txt &&
+   CC="$CC_PARAM" cmake . -DBUILD_SHARED_LIBS=ON -DBUILD_STATIC_LIBS=OFF -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_C_FLAGS_RELEASE="$DEFAULT_FLAGS" && make -j8 &&
+   cp liblz4.so $OUT_PARAM)
+}
+
+function compile_zlib {
+  CC_PARAM=$1
+  OUT_PARAM=$2
+ 
+  (cd extsrc/zlib-ng && git clean -fdx &&
+   cp $PROJECT_ROOT/native-cmakes/CMakeLists.libz.txt CMakeLists.txt &&
+   CC="$CC_PARAM" cmake . -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_C_FLAGS_RELEASE="$DEFAULT_FLAGS" -DZLIB_COMPAT=1 -DARCH=i686 && make -j8 &&
+   cp libz.so $OUT_PARAM)
+}
+
+function compile_lzma {
+  CC_PARAM=$1
+  OUT_PARAM=$2
+  
+  (cd extsrc/xz && git clean -fdx &&
+   ./autogen.sh && CC="$CC_PARAM" CFLAGS="$DEFAULT_FLAGS" ./configure --host=i686-pc-linux-gnu && make V=0 -j8 &&
+   cp src/liblzma/.libs/liblzma.so $OUT_PARAM)
+}
+
+
+compile_lzo2 "$CC_X86" "$X86_OUT"
+compile_lzo2 "$CC_X64" "$X64_OUT"
+
+compile_bzip2 "$CC_X86" "$X86_OUT"
+compile_bzip2 "$CC_X64" "$X64_OUT"
+
+compile_lz4 "$CC_X86" "$X86_OUT"
+compile_lz4 "$CC_X64" "$X64_OUT"
+
+compile_zlib "$CC_X86" "$X86_OUT"
+compile_zlib "$CC_X64" "$X64_OUT"
+
+compile_lzma "$CC_X86" "$X86_OUT"
+compile_lzma "$CC_X64" "$X64_OUT"
 
 echo Stripping shared objects...
 echo Before strip: $(du -chs $X86_OUT/ $X64_OUT/ | tail -1 | cut -f 1)
